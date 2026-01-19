3cf6518580f391a5b0d0d58f61e8f3400a239f5e
Justin Herndon <jherndon111@gmail.com>
2026-01-19 15:29:03 -0600

    removing template section

--- transcode/cli.py
+++ transcode/cli.py
@@ -6,16 +6,14 @@ import click
 from transcode import __version__
 from transcode.resources import read_help_text
 
-
 @click.command()
 @click.option("--name", prompt="Your name", help="The person to greet.")
 @click.option("--count", default=1, help="Number of greetings.")
-@click.version_option(__version__, prog_name="zamazingo")
+@click.version_option(__version__, prog_name="transcode")
 def main(name: str, count: int) -> None:
     """Run the CLI program."""
     for _ in range(count):
         click.secho(f"Hello {name}! I am transcode v{__version__}.", bold=True)
 
     click.echo()
-
     click.secho(read_help_text(), fg="yellow")

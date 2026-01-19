e3d1edfb1ed92bace86de7856ccf1f1ae5829743
Justin Herndon <jherndon111@gmail.com>
2026-01-19 13:09:23 -0600

    tried using https://github.com/vst/nix-flake-templates/tree/main as a flake template, still need some changes to make it my own

--- flake.nix
+++ flake.nix
@@ -1,43 +1,115 @@
 {
-  description = "Transcoding program with ffmpeg bindings";
+  description = "A Python Package";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      inherit (nixpkgs) lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        ## Import nixpkgs:
+        pkgs = import nixpkgs { inherit system; };
+
+        ## Read pyproject.toml file:
+        pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+
+        ## Get project specification:
+        project = pyproject.project;
+
+        ## Get the package:
+        package = pkgs.python3Packages.buildPythonPackage {
+          ## Set the package name:
+          pname = project.name;
+
+          ## Inherit the package version:
+          inherit (project) version;
+
+          ## Set the package format:
+          format = "pyproject";
+
+          ## Set the package source:
+          src = ./.;
+
+          ## Specify the build system to use:
+          build-system = with pkgs.python3Packages; [
+            setuptools
+          ];
+
+          ## Specify test dependencies:
+          nativeCheckInputs = [
+            ## Python dependencies:
+            pkgs.python3Packages.mypy
+            pkgs.python3Packages.nox
+            pkgs.python3Packages.pytest
+            pkgs.python3Packages.ruff
+
+            ## Non-Python dependencies:
+            pkgs.taplo
+          ];
+
+          ## Define the check phase:
+          checkPhase = ''
+            runHook preCheck
+            nox
+            runHook postCheck
+          '';
+
+          ## Specify production dependencies:
+          propagatedBuildInputs = [
+            pkgs.python3Packages.click
+          ];
+        };
+
+        ## Make our package editable:
+        editablePackage = pkgs.python3.pkgs.mkPythonEditablePackage {
+          pname = project.name;
+          inherit (project) scripts version;
+          root = "$PWD";
+        };
+      in
+      {
+        ## Project packages output:
+        packages = {
+          "${project.name}" = package;
+          default = self.packages.${system}.${project.name};
+        };
+
+        ## Project development shell output:
+        devShells = {
           default = pkgs.mkShell {
-            packages = [
-              pkgs.python3
-              pkgs.uv
-              pkgs.ffmpeg
+            inputsFrom = [
+              package
             ];
 
-            env = lib.optionalAttrs pkgs.stdenv.isLinux {
-              # Python libraries often load native shared objects using dlopen(3).
-              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-            };
+            buildInputs = [
+              #################
+              ## OUR PACKAGE ##
+              #################
 
-            shellHook = ''
-              unset PYTHONPATH
-              uv sync
-              . .venv/bin/activate
-            '';
+              editablePackage
+
+              #################
+              # VARIOUS TOOLS #
+              #################
+
+              pkgs.python3Packages.build
+              pkgs.python3Packages.ipython
+
+              ####################
+              # EDITOR/LSP TOOLS #
+              ####################
+
+              # LSP server:
+              pkgs.python3Packages.python-lsp-server
+
+              # LSP server plugins of interest:
+              pkgs.python3Packages.pylsp-mypy
+              pkgs.python3Packages.pylsp-rope
+              pkgs.python3Packages.python-lsp-ruff
+            ];
           };
-        }
-      );
-    };
+        };
+      });
 }

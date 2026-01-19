630db30ce7513c049bd51b62fb74e7334257560e
Justin Herndon <jherndon111@gmail.com>
2026-01-19 15:25:57 -0600

    removing extra template code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,8 +4,8 @@
 
 from click.testing import CliRunner
 
-from zamazingo import __version__
-from zamazingo.cli import main
+from transcode import __version__
+from transcode.cli import main
 
 
 def test_cli() -> None:
@@ -13,4 +13,4 @@ def test_cli() -> None:
     runner = CliRunner()
     result = runner.invoke(main, ["--name", "test", "--count", "1"])
     assert result.exit_code == 0
-    assert result.output.startswith(f"Hello test! I am zamazingo v{__version__}.\n")
+    assert result.output.startswith(f"Hello test! I am transcode v{__version__}.\n")
